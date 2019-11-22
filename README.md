f5e050130894aecba26e08893ea7d823a0b0f31c
Chukwuma Okere <chukwumaokere@Chukwumas-Mac.local>
2019-11-22 17:05:52 -0500

    updating ios

--- README.md
+++ README.md
@@ -14,3 +14,17 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+
+
+
+To add any project from your folder to GIT
+
+git init
+git add . // Adds the files in the local repository and stages them for commit. To unstage a file, use 'git reset HEAD YOUR-FILE'.
+git commit -m "First commit" // Commits the tracked changes and prepares them to be pushed to a remote repository. To remove this commit and modify the file, use 'git reset --soft HEAD~1' and commit and add the file again.
+
+git remote add origin remote repository URL //(remote repository URL here will be the GitHub URL of your project )
+git remote -v //Verifies the new remote URL
+git push -u origin master //Pushes the changes in your local repository up to the remote repository you specified as the origin
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
-
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:image_crop/image_crop.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
 
+//import 'crop_image.dart';
+import 'package:http/http.dart' as http;
 const kAndroidUserAgent =
     'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
 
 //String selectedUrl = 'https://damp-coast-35782.herokuapp.com';
-String selectedUrl = 'https://devl06.borugroup.com/cokere/mva-phoneapp/#!/';
+String selectedUrl = 'https://devl06.borugroup.com/mvadev/mva-phoneapp/#!/';
 //String selectedUrl = 'https://devl06.borugroup.com/cokere/buttontest/';
 
 // ignore: prefer_collection_literals
@@ -20,31 +26,102 @@ final Set<JavascriptChannel> jsChannels = [
           //MyApp.startBarcode();
           print(message.message);
           sBarcode(MyApp());
+        } else if(message.message=='scanAssets'){
+          print(message.message);
+          sAssetBarcode(MyApp());
         }
-      }),
+        else if(message.message=='captureImage'){
+          print(message.message);
+          //runApp(MyApps());
+          takeImage(MyApps());
+        }}),
 ].toSet();
 
+
 sBarcode(someVal) async {
-  String bCode = await FlutterBarcodeScanner.scanBarcode("#ff6666", "Cancel", true);
+  String bCode = await FlutterBarcodeScanner.scanBarcode("#ff6666", "Cancel", true, ScanMode.DEFAULT);
   print(bCode);
   someVal.enterBarcode(bCode);
   return;
 }
+sAssetBarcode(someVal) async {
+  String bCode = await FlutterBarcodeScanner.scanBarcode("#ff6666", "Cancel", true, ScanMode.DEFAULT);
+  print(bCode);
+  someVal.enterBarcodeAsset(bCode);
+  return;
+}
+takeImage(someVal) async {
+  Future opImg = _MyAppState()._openImage();
+  opImg.then((image) => uploadImage(MyApp(),image) ).catchError((error) => debugPrint(error));
+  //MyApps().createState();
+
+}
+uploadImage(someVal,file) async{
+  debugPrint('$file');
+  someVal._upload(file);
+}
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatelessWidget {
+
+  final cropKey = GlobalKey<CropState>();
+
   final flutterWebViewPlugin = FlutterWebviewPlugin();
-  /*
-  startBarcode() async {
-    String barcodeScanRes = await sBarcode();
-    flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumber').value=" + barcodeScanRes);
-  }
-  */
   enterBarcode(barc) {
-    flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumber').value=" + barc);
+    flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumber').value=" + barc + ";");
+	flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumber').dispatchEvent(event)");
+  }
+  enterCamera(barc) {
+    flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').value=" + barc);
+  }
+  enterBarcodeAsset(barc) {
+    flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').value=" + barc);
+    flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').dispatchEvent(new KeyboardEvent('keyup'))");
   }
 
+  void _upload(file) {
+    if (file == null) {
+      //String base64Image = base64Encode(file.readAsBytesSync());
+      //String fileName = file.path.split("/").last;
+	flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').value='0'");
+      flutterWebViewPlugin.evalJavascript("eMessageTwo()");
+
+    } else{
+      final String phpEndPoint = 'http://devl06.borugroup.com/mvadev/phoneapi/scanImage.php';
+      String base64Image = base64Encode(file.readAsBytesSync());
+      String fileName = file.path.split("/").last;
+      flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').value= 'Fetching...'");
+
+      http.post(phpEndPoint, body: {
+        "image": base64Image,
+        "name": fileName,
+      }).then((res) {
+	
+        //flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').value= 6767");
+        print(res.statusCode);
+        String statusCode = res.body;
+        final jsonResponse = json.decode(res.body);
+        //String message =statusCode.message;
+	/*
+	if (jsonResponse['message'] == '' || !jsonResponse['message'] || jsonResponse['message'] == ' '){
+		flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').value='1'");
+		flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').value=''");
+		flutterWebViewPlugin.evalJavascript("eMessage()");
+	}
+	*/
+        flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').value="+jsonResponse['message']);
+        flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').dispatchEvent(new KeyboardEvent('keyup'))");
+
+      }).catchError((err) {
+        //print(err);
+        flutterWebViewPlugin.evalJavascript("document.getElementById('barcodenumberasset').value= 11"+err);
+
+      });
+    }
+
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -53,7 +130,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       routes: {
-       // '/': (_) => const MyHomePage(title: 'Flutter WebView Demo'),
+        // '/': (_) => const MyHomePage(title: 'Flutter WebView Demo'),
         '/': (_) {
           return WebviewScaffold(
             url: selectedUrl,
@@ -143,49 +220,49 @@ class _MyHomePageState extends State<MyHomePage> {
 
     _onProgressChanged =
         flutterWebViewPlugin.onProgressChanged.listen((double progress) {
-      if (mounted) {
-        setState(() {
-          _history.add('onProgressChanged: $progress');
+          if (mounted) {
+            setState(() {
+              _history.add('onProgressChanged: $progress');
+            });
+          }
         });
-      }
-    });
 
     _onScrollYChanged =
         flutterWebViewPlugin.onScrollYChanged.listen((double y) {
-      if (mounted) {
-        setState(() {
-          _history.add('Scroll in Y Direction: $y');
+          if (mounted) {
+            setState(() {
+              _history.add('Scroll in Y Direction: $y');
+            });
+          }
         });
-      }
-    });
 
     _onScrollXChanged =
         flutterWebViewPlugin.onScrollXChanged.listen((double x) {
-      if (mounted) {
-        setState(() {
-          _history.add('Scroll in X Direction: $x');
+          if (mounted) {
+            setState(() {
+              _history.add('Scroll in X Direction: $x');
+            });
+          }
         });
-      }
-    });
 
     _onStateChanged =
         flutterWebViewPlugin.onStateChanged.listen((WebViewStateChanged state) {
           print(state.type);
-      if (mounted) {
-        setState(() {
-          _history.add('onStateChanged: ${state.type} ${state.url}');
+          if (mounted) {
+            setState(() {
+              _history.add('onStateChanged: ${state.type} ${state.url}');
+            });
+          }
         });
-      }
-    });
 
     _onHttpError =
         flutterWebViewPlugin.onHttpError.listen((WebViewHttpError error) {
-      if (mounted) {
-        setState(() {
-          _history.add('onHttpError: ${error.code} ${error.url}');
+          if (mounted) {
+            setState(() {
+              _history.add('onHttpError: ${error.code} ${error.url}');
+            });
+          }
         });
-      }
-    });
   }
 
   @override
@@ -227,7 +304,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       0.0, 0.0, MediaQuery.of(context).size.width, 300.0),
                   userAgent: kAndroidUserAgent,
                   invalidUrlRegex:
-                      r'^(https).+(twitter)', // prevent redirecting to twitter when user click on its icon in flutter website
+                  r'^(https).+(twitter)', // prevent redirecting to twitter when user click on its icon in flutter website
                 );
               },
               child: const Text('Open Webview (rect)'),
@@ -257,7 +334,7 @@ class _MyHomePageState extends State<MyHomePage> {
             RaisedButton(
               onPressed: () {
                 final future =
-                    flutterWebViewPlugin.evalJavascript(_codeCtrl.text);
+                flutterWebViewPlugin.evalJavascript(_codeCtrl.text);
                 future.then((String result) {
                   setState(() {
                     _history.add('eval: $result');
@@ -290,5 +367,135 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+}
+//git pull git@github.com:vixur-legacy/todaysepicure.git master
+
+class MyApps extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApps> {
+  final cropKey = GlobalKey<CropState>();
+  File _file;
+  File _sample;
+  File _lastCropped;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _file?.delete();
+    _sample?.delete();
+    _lastCropped?.delete();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Container(
+          color: Colors.black,
+          padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
+          child: _sample == null ? _buildOpeningImage() : _buildCroppingImage(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOpeningImage() {
+    return Center(child: _buildOpenImage());
+  }
+
+  Widget _buildCroppingImage() {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Crop.file(_sample, key: cropKey),
+        ),
+        Container(
+          padding: const EdgeInsets.only(top: 20.0),
+          alignment: AlignmentDirectional.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              /* FlatButton(
+                child: Text(
+                  'Crop Image',
+                  /* style: Theme.of(context)
+                      .textTheme
+                      .button
+                      .copyWith(color: Colors.white), */
+                ),
+                onPressed: () => (),
+              ), */
+              _buildOpenImage(),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildOpenImage() {
+    return FlatButton(
+      child: Text(
+        'Open Camera',
+        //style: Theme.of(context).textTheme.button.copyWith(color: Colors.white),
+      ),
+      onPressed: () => _openImage(),
+    );
+  }
+
+  Future<File> _openImage() async {
+    final file = await ImagePicker.pickImage(source: ImageSource.camera);
+    final sample = await ImageCrop.sampleImage(
+      file: file,
+      preferredWidth: 4096,
+      preferredHeight: 1024,
+    );
+
+    _sample?.delete();
+    _file?.delete();
+
+    /* setState(() {
+      this._sample = sample;
+      this._file = file;
+    });  */
+    this._sample = sample;
+    this._file = file;
+    final area = Rect.fromLTWH( 0.0, 0.0, 1.0, 1.0);
+    final img = await ImageCrop.cropImage(
+      file: this._file,
+      area: area,
+    );
+
+    File croppedFile = await ImageCropper.cropImage(
+        sourcePath: img.path,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9
+        ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.blueGrey,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          //minimumAspectRatio: 1.0,
+		showCancelConfirmationDialog: true,
+        )
+    );
+
+    //_buildCroppingImage();
+    return croppedFile;
+
+    // _cropImage(this._file);
+
   }
 }
